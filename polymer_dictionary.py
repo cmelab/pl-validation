9174e6cd3a615893273470fbfeb25bc2222e3a1c
jbieri <jacobieri909@gmail.com>
2025-05-30 11:03:10 -0600

    Added some comments

---
+++ polymer_dictionary.py
@@ -0,0 +1,17 @@
+# Dictionary of polymers and their key parameters for sims
+
+bond_indices = {
+    "PCPDTPT_nC16": {"Bond indices": [121,122], "Orientations":[[1,0,0],[-1,0,0]], "Density": 0.05},
+    "PCPDTFBT_C1_BO": {"Bond indices": [196,197], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDTFBT_C5_BO": {"Bond indices": [244,245], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDT_PT_eneHD": {"Bond indices": [122,123], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.005},
+    "PCPDTPT_eneODD": {"Bond indices": [12,147], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDTFBT_C3_BO": {"Bond indices": [220,221], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDTFBT_C11_BO": {"Bond indices": [316,317], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.005},
+    "PIDTBT_nC16": {"Bond indices": [229,230], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PIDTFBT_C11_BO": {"Bond indices": [602,603], "Orientations":[[1,0,0],[-1,0,0]], "Density": 0.005},
+    "PIDTCPDT_C11BO": {"Bond indices": [437,438], "Orientations":[[-1,0,0],[1,0,0]],"Density": 0.05},
+    "PCPDTPT_HD": {"Bond indices": [121,122], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDTPT_ODD": {"Bond indices": [145,146], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.05},
+    "PCPDTFBT_C4_BO": {"Bond indices": [232,233], "Orientations":[[-1,0,0],[1,0,0]], "Density": 0.01}
+}
